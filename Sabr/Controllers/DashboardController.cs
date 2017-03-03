b739e6fba1d6783da4a03c54e8d45e687869fad2
seanob30 <seanobrien2014@gmail.com>
2017-03-03 00:37:06 -0600

    Added Roster on MyTeam View

--- Sabr/Controllers/DashboardController.cs
+++ Sabr/Controllers/DashboardController.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Sabr.Models;
+using Sabr.ViewModels;
 
 namespace Sabr.Controllers
 {
     public class DashboardController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public DashboardController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Dashboard
         public ActionResult Index()
         {
@@ -16,7 +31,17 @@ namespace Sabr.Controllers
 
         public ActionResult MyTeam()
         {
-            return View();
+            var currentUserName = User.Identity.GetUserName();
+            var allPlayers = _context.Players.ToList();
+            var user = _context.Users.FirstOrDefault(m => m.UserName == currentUserName);
+            var userTeam = _context.Teams.FirstOrDefault(m => m.Id == user.TeamId);
+            var viewModel = new DashboardViewModels.MyTeamViewModel
+            {
+                CurrentUser = user,
+                Team = userTeam,
+                Players = allPlayers
+            };
+            return View(viewModel);
         }
 
         public ActionResult League()
---
+++ Sabr/ViewModels/DashboardViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sabr.Models;
+
+namespace Sabr.ViewModels
+{
+    public class DashboardViewModels
+    {
+        public class MyTeamViewModel
+        {
+            public ApplicationUser CurrentUser { get; set; }
+            public Team Team { get; set; }
+            public IEnumerable<Player> Players { get; set; }    
+        }
+    }
+}
