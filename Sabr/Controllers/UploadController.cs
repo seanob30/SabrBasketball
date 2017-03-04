1e8f65e6ce9cb27ce193e6dea794b280db32974b
seanob30 <seanobrien2014@gmail.com>
2017-03-04 00:12:27 -0600

    Fixed minor bugs

---
+++ Sabr/Controllers/UploadController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Sabr.Models;
+using Sabr.ViewModels;
+
+namespace Sabr.Controllers
+{
+    public class UploadController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
